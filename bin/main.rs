994d27f0dc5820686e91ef2c8f5277388d42e2d2
justincremer <jacremer@live.com>
2021-07-10 14:36:16 -0700

    adds async auth method

--- bin/main.rs
+++ bin/main.rs
@@ -1,9 +1,19 @@
 extern crate noclist;
+extern crate tokio;
 
-use noclist::checksum;
+// use noclist::checksum;
+use noclist::HttpClient;
 
-fn main() {
-    let token = "F47D071E3E94B0A6BCCC44B47CD8CA44";
-    let path = "/user";
-    let checksum = checksum(token, path);
+#[tokio::main]
+async fn main() {
+    run().await;
+
+    // let token = "F47D071E3E94B0A6BCCC44B47CD8CA44";
+    // let path = "/user";
+    // let checksum = checksum(token, path);
+}
+
+async fn run() {
+    let client = HttpClient::new("http://0.0.0.0:8888");
+    client.auth().await;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,80 @@
 extern crate reqwest;
 extern crate sha2;
 
+use std::borrow::{Borrow, BorrowMut};
+use std::convert::TryInto;
+
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
-use reqwest::{Client, Url};
+use reqwest::{Client, Response, Url};
 use sha2::{Digest, Sha256};
 
-struct HttpClient {
-    client: Client,
-    base_url: String,
-	checksum: String,
+pub struct HttpClient {
+    pub client: Client,
+    pub base_url: String,
+    pub token: String,
 }
+
+type HttpResponse = Result<Response, reqwest::Error>;
 
 impl HttpClient {
-    pub fn new(base_url: String) -> Self {
-        let client = Client::new();
-		let checksum = String::new();
-        HttpClient { client, base_url, checksum }
+    pub fn new(base_url: &str) -> Self {
+        HttpClient {
+            client: Client::new(),
+            base_url: String::from(base_url),
+            token: String::new(),
+        }
     }
 
-    pub async fn auth(&self) -> Option<String> {
-        let url = match Url::parse(&format!("{}{}", self.base_url, "/auth")) {
-			Ok(v) => Some(v),
-			_ => {
-				eprintln!("failed to parse url");
-				None
-			}
-		};
-		
-        self.client.get(url.unwrap()).send()?
+    pub async fn auth(&mut self) {
+        let url = Url::parse(&format!("{}{}", self.base_url, "/auth")).unwrap();
+        match self.client.get(url).send().await {
+            Ok(r) => {
+                self.token = r
+                    .headers()
+                    .get("badsec-authentication-token")
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .replace("-", "")
+                    .to_string();
+            }
+            Err(e) => eprintln!("failed to authenticate, {}", e),
+        };
     }
 
-    pub async fn get_users(&self, base_url: &str, checksum: &'static String) {
-        let url = Url::parse(&format!("{}{}", self.base_url, "/users"));
-        let headers = construct_headers(&checksum);
+    pub async fn get_users(&self) {
+        let path = "/users";
+        let url = Url::parse(&format!("{}{}", self.base_url, path)).unwrap();
+        // let checksum = create_checksum(self.token.clone(), String::from(path));
+        let headers = self.construct_headers(&path);
+        match self.client.get(url).headers(headers).send().await {
+            Ok(r) => println!("{:?}", r),
+
+            Err(e) => eprintln!("failed to fetch users, {}", e),
+        };
     }
-}
 
-pub fn checksum<'a>(token: &str, path: &str) -> String {
-    let mut hasher = Sha256::new();
-    let input = format!("{}{}", token, path);
+    fn construct_headers(&self, path: &str) -> HeaderMap {
+        let mut headers = HeaderMap::new();
+        let checksum = self.create_checksum(path);
 
-    hasher.update(input);
-    hasher
-        .finalize()
-        .iter()
-        .fold(String::new(), |acc, i| acc + i.to_string().as_str())
-}
+        headers.insert(
+            HeaderName::from_static("X-Request-Checksum"),
+            HeaderValue::from_static(checksum.as_str()),
+        );
+        headers
+    }
 
-fn construct_headers(checksum: &'static String) -> HeaderMap {
-    let mut headers = HeaderMap::new();
-    headers.insert(
-        HeaderName::from_static("X_Request_Checksum"),
-        HeaderValue::from_static(checksum.as_str()),
-    );
-    headers
+    fn create_checksum<'a>(&self, path: &str) -> String {
+        let mut hasher = Sha256::new();
+        let input = format!("{}{}", self.token, path);
+
+        hasher.update(input);
+        hasher
+            .finalize()
+            .iter()
+            .fold(String::new(), |acc, i| acc + i.to_string().as_str())
+    }
 }
 
 // pub fn bytes_to_str<'a>(i: &'a [u8]) -> Result<&'a str, Utf8Error> {
