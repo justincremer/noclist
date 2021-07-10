a1a59f2d34ceca92c1bdef0762c8cffe3609b33b
justincremer <jacremer@live.com>
2021-07-10 15:13:01 -0700

    fixes async methods

--- bin/main.rs
+++ bin/main.rs
@@ -1,19 +1,14 @@
 extern crate noclist;
-extern crate tokio;
 
-// use noclist::checksum;
 use noclist::HttpClient;
 
 #[tokio::main]
 async fn main() {
-    run().await;
+    let mut client = HttpClient::new("http://0.0.0.0:8888");
+    client.auth().await;
+    client.get_users().await;
 
     // let token = "F47D071E3E94B0A6BCCC44B47CD8CA44";
     // let path = "/user";
     // let checksum = checksum(token, path);
-}
-
-async fn run() {
-    let client = HttpClient::new("http://0.0.0.0:8888");
-    client.auth().await;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
+#[macro_use]
 extern crate reqwest;
 extern crate sha2;
 
-use std::borrow::{Borrow, BorrowMut};
-use std::convert::TryInto;
-
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
-use reqwest::{Client, Response, Url};
+use reqwest::{Client, Url};
 use sha2::{Digest, Sha256};
 
 pub struct HttpClient {
@@ -14,7 +12,7 @@ pub struct HttpClient {
     pub token: String,
 }
 
-type HttpResponse = Result<Response, reqwest::Error>;
+// type HttpResponse = Result<Response, reqwest::Error>;
 
 impl HttpClient {
     pub fn new(base_url: &str) -> Self {
@@ -45,11 +43,15 @@ impl HttpClient {
     pub async fn get_users(&self) {
         let path = "/users";
         let url = Url::parse(&format!("{}{}", self.base_url, path)).unwrap();
-        // let checksum = create_checksum(self.token.clone(), String::from(path));
-        let headers = self.construct_headers(&path);
-        match self.client.get(url).headers(headers).send().await {
-            Ok(r) => println!("{:?}", r),
 
+        match self
+            .client
+            .get(url)
+            .headers(self.construct_headers(&path))
+            .send()
+            .await
+        {
+            Ok(r) => println!("{:?}", r),
             Err(e) => eprintln!("failed to fetch users, {}", e),
         };
     }
@@ -59,24 +61,24 @@ impl HttpClient {
         let checksum = self.create_checksum(path);
 
         headers.insert(
-            HeaderName::from_static("X-Request-Checksum"),
-            HeaderValue::from_static(checksum.as_str()),
+            HeaderName::from_static("x-request-checksum"),
+            HeaderValue::from_static(Box::leak(checksum)),
         );
         headers
     }
 
-    fn create_checksum<'a>(&self, path: &str) -> String {
+    // TODO: fix utf8 consumtption of hash
+    fn create_checksum<'a>(&self, path: &str) -> Box<String> {
         let mut hasher = Sha256::new();
         let input = format!("{}{}", self.token, path);
 
         hasher.update(input);
-        hasher
-            .finalize()
-            .iter()
-            .fold(String::new(), |acc, i| acc + i.to_string().as_str())
+
+        Box::from(
+            hasher
+                .finalize()
+                .iter()
+                .fold(String::new(), |acc, i| acc + i.to_string().as_str()),
+        )
     }
 }
-
-// pub fn bytes_to_str<'a>(i: &'a [u8]) -> Result<&'a str, Utf8Error> {
-//     str::from_utf8(i)
-// }
