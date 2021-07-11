0db8eaec5479087ea7993e5128a516ca82dd78c0
justincremer <jacremer@live.com>
2021-07-11 01:02:59 -0700

    adds hex crate for properly encoding checksum

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+extern crate hex;
 extern crate reqwest;
 extern crate sha2;
 
+use hex::ToHex;
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use reqwest::{Client, Url};
 use sha2::{Digest, Sha256};
@@ -23,6 +25,17 @@ impl HttpClient {
         }
     }
 
+    pub async fn get_users(&self) {
+        let path = "/users";
+        let url = Url::parse(&format!("{}{}", self.base_url, path)).unwrap();
+        let headers = self.construct_headers(&path);
+
+        match self.client.get(url).headers(headers).send().await {
+            Ok(r) => println!("{:?}", r),
+            Err(e) => eprintln!("failed to fetch users, {}", e),
+        };
+    }
+
     pub async fn auth(&mut self) {
         let url = Url::parse(&format!("{}{}", self.base_url, "/auth")).unwrap();
         match self.client.get(url).send().await {
@@ -33,47 +46,30 @@ impl HttpClient {
                     .unwrap()
                     .to_str()
                     .unwrap()
-                    .replace("-", "")
                     .to_string();
             }
             Err(e) => eprintln!("failed to authenticate, {}", e),
         };
     }
 
-    pub async fn get_users(&self) {
-        let path = "/users";
-        let url = Url::parse(&format!("{}{}", self.base_url, path)).unwrap();
-        let headers = self.construct_headers(&path);
-
-        match self.client.get(url).headers(headers.unwrap()).send().await {
-            Ok(r) => println!("{:?}", r),
-            Err(e) => eprintln!("failed to fetch users, {}", e),
-        };
+    fn construct_headers(&self, path: &str) -> HeaderMap {
+        let checksum = self.create_sha256_checksum(path);
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            HeaderName::from_static("x-request-checksum"),
+            HeaderValue::from_static(Box::leak(checksum)),
+        );
+        headers
     }
 
-    fn construct_headers(&self, path: &str) -> Result<HeaderMap, &str> {
-        match self.create_sha256_checksum(path) {
-            Ok(v) => {
-                let mut headers = HeaderMap::new();
-                headers.insert(
-                    HeaderName::from_static("x-request-checksum"),
-                    HeaderValue::from_static(Box::leak(v)),
-                );
-                Ok(headers)
-            }
-            Err(e) => Err(e),
-        }
-    }
-
-    fn create_sha256_checksum<'a>(&self, path: &str) -> Result<Box<String>, &str> {
+    fn create_sha256_checksum<'a>(&self, path: &str) -> Box<String> {
         let mut hasher = Sha256::new();
-        let input = self.token.clone() + path;
+        let input = self.token.to_owned() + path;
         let mut buf: [u8; 32] = [0; 32];
-        hasher.update(input.as_bytes());
+
+        hasher.update(input);
         buf.copy_from_slice(&hasher.finalize());
-        match str::from_utf8(&buf) {
-            Ok(s) => Ok(Box::from(s.to_string())),
-            Err(e) => Err(format!("failed to parse buffer to utf8, {}", e)),
-        }
+
+        Box::from(hex::encode(&buf))
     }
 }
