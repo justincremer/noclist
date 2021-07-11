d76175bf664265ef44a04678553e11bbeedd41b6
justincremer <jacremer@live.com>
2021-07-10 18:13:13 -0700

    modifies checksum method

--- bin/main.rs
+++ bin/main.rs
@@ -7,8 +7,4 @@ async fn main() {
     let mut client = HttpClient::new("http://0.0.0.0:8888");
     client.auth().await;
     client.get_users().await;
-
-    // let token = "F47D071E3E94B0A6BCCC44B47CD8CA44";
-    // let path = "/user";
-    // let checksum = checksum(token, path);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 extern crate reqwest;
 extern crate sha2;
 
-use sha2::{Digest, Sha256};
-
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use reqwest::{Client, Url};
-
-use std::{error, str};
+use sha2::{Digest, Sha256};
+use std::str;
 
 pub struct HttpClient {
     pub client: Client,
@@ -45,14 +43,9 @@ impl HttpClient {
     pub async fn get_users(&self) {
         let path = "/users";
         let url = Url::parse(&format!("{}{}", self.base_url, path)).unwrap();
+        let headers = self.construct_headers(&path);
 
-        match self
-            .client
-            .get(url)
-            .headers(self.construct_headers(&path))
-            .send()
-            .await
-        {
+        match self.client.get(url).headers(headers).send().await {
             Ok(r) => println!("{:?}", r),
             Err(e) => eprintln!("failed to fetch users, {}", e),
         };
@@ -60,21 +53,29 @@ impl HttpClient {
 
     fn construct_headers(&self, path: &str) -> HeaderMap {
         let mut headers = HeaderMap::new();
-        let checksum = self.create_checksum::<Sha256>(path);
+        let checksum = self.create_sha256_checksum(path);
 
         headers.insert(
             HeaderName::from_static("x-request-checksum"),
-            HeaderValue::from_static(Box::leak(checksum)),
+            HeaderValue::from_static(Box::leak(checksum.unwrap())),
         );
         headers
     }
 
-    fn create_checksum<'a, D: Digest>(&self, path: &str) -> Box<String> {
-        let mut hasher = D::new();
+    fn create_sha256_checksum<'a>(&self, path: &str) -> Option<Box<String>> {
+        let mut hasher = Sha256::new();
         let input = self.token.clone() + path;
         hasher.update(input.as_bytes());
-        let mut buf: Vec<u8> = Vec::new();
+
+        let mut buf: [u8; 32] = [0; 32];
         buf.copy_from_slice(&hasher.finalize());
-        Box::from(String::from_utf8(buf).unwrap())
+        match str::from_utf8(&buf) {
+            Ok(s) => Some(Box::from(s.to_string())),
+            Err(e) => {
+                eprintln!("failed to parse buffer to utf8, {}", e);
+                None
+            }
+        }
+        // hasher.finalize()
     }
 }
