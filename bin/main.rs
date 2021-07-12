404643232c333cca89cc46b92c3be2deedfaa77a
justincremer <jacremer@live.com>
2021-07-11 17:53:49 -0700

    refactors binary for readable retries on both auth and users endpoints

--- bin/main.rs
+++ bin/main.rs
@@ -1,6 +1,6 @@
 extern crate noclist;
 
-use noclist::http::{ensure_success, HttpClient};
+use noclist::http::{check_success, HttpClient};
 use noclist::users::Users;
 
 use std::process::exit;
@@ -8,26 +8,49 @@ use std::process::exit;
 #[tokio::main]
 async fn main() {
     let mut client = HttpClient::new("http://0.0.0.0:8888");
+    // Initialize empty user vector
     let mut users: Users = Default::default();
 
-    // Authenticate client
-    ensure_success(&client.auth().await);
-
-    // Fetch users, attempting 3 times, and smoothly exiting.
+    // Authenticate client, attempting 3 times, and smoothly exiting.
     // I would normally write this generically and recursively,
     // but there is no need for a simple appliction such as this.
-    let mut acc = 0;
-    while acc < 3 {
+    let mut success = false;
+    for _ in 0..3 {
+        match check_success(&client.auth().await) {
+            true => {
+                success = true;
+                break;
+            }
+            _ => {}
+        }
+    }
+
+    if !success {
+        eprintln!("failed to authenticate client session");
+        exit(1);
+    };
+
+    success = false;
+    // Fetch users, attempting 3 times, and smoothly exiting.
+    for _ in 0..3 {
         let res = client.get_users().await;
         // Match over success of users response, populating the users vector on success
         match users.populate_from_response(res).await {
             Ok(_) => {
-                println!("{}", users);
-                exit(0);
+                success = true;
+                break;
             }
-            Err(_) => {}
+            _ => {}
         }
-        acc += 1;
     }
-    exit(1);
+
+    match success {
+        true => {
+            println!("{}", users);
+            exit(0);
+        }
+        false => {
+            exit(1);
+        }
+    };
 }
